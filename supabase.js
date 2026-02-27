dd5632c82583335a99624d3bbc9c89436efcbe3f
stromausfall-hotline1234 <info.hajdarovacdennis@gmail.com>
2026-02-27 23:28:39 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
 
+// ENV Variablen, gesetzt in .env (nicht pushen!)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
